b0f60d23a79b652a8d798418c0656e328719f506
Shannon <shannonmlance@gmail.com>
2019-06-08 18:42:18 -0700

    added bootstrap, jquery, popper.js, and jquery-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'zelda-app';
+  ngAfterContentInit() {
+    console.log("jquery is running.");
+
+    $(function() {
+        $("#accordion").accordion({
+            active: false,
+            collapsible: true,
+            heightStyle: "content"
+        });
+    });
+  }
 }

786efcd48059d08f9380d3624b003f8ddd6e306e
Shannon <shannonmlance@gmail.com>
2019-06-08 19:00:30 -0700

    added navigation header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// import { HomeComponent } from './home/home.component';
+// import { AvatarComponent } from './avatar/avatar.component';
+// import { WalkthroughsLandingPageComponent } from './walkthroughs-landing-page/walkthroughs-landing-page.component';
+
+const routes: Routes = [
+    // { path: '', pathMatch: 'full', redirectTo: '/home' },
+    // { path: 'home', component: HomeComponent },
+    // { path: 'avatar', component: AvatarComponent },
+    // { path: 'walkthroughs-landing-page', component: WalkthroughsLandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AvatarComponent } from './avatar/avatar.component';
+// import { HomeComponent } from './home/home.component';
+// import { WalkthroughsLandingPageComponent } from './walkthroughs-landing-page/walkthroughs-landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // AvatarComponent,
+    // HomeComponent,
+    // WalkthroughsLandingPageComponent
   ],
   imports: [
     BrowserModule,
